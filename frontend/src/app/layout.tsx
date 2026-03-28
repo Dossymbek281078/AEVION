abf342773e5ffb04429a800fd7c154c99a77daa3
Dossymbek281078 <yahiin1978@gmail.com>
2026-03-28 23:39:27 +0500

    Fix hydration error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -34,8 +34,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
       >
